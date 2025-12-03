b66c26decea5727e0e05e647783d6c24075f4ad2
Namra Maniar <namra4122@gmail.com>
2025-12-04 02:09:10 +0530

    added the doubly linked list python

--- linked_list/doubly.py
+++ linked_list/doubly.py
@@ -1,16 +1,18 @@
 class Node:
-    def __init__(self, value:int):
-        self.value = value
+    def __init__(self, data: int):
+        self.data = data
         self.next = None
+        self.prev = None
 
-class SinglyLL:
+
+class DoublyLL:
     def __init__(self):
         self.head = None
 
-    def add(self, value: int):
-        new_node = Node(value)
+    def add(self, data: int):
+        new_node = Node(data)
 
-        if self.head == None:
+        if self.head is None:
             self.head = new_node
             return
 
@@ -19,13 +21,16 @@ class SinglyLL:
             curr = curr.next
 
         curr.next = new_node
+        new_node.prev = curr
         return
 
-    def insert(self, index: int, value: int) -> None:
-        new_node = Node(value)
+    def insert(self, index: int, data: int):
+        new_node = Node(data)
 
         if index == 0:
             new_node.next = self.head
+            if self.head:
+                self.head.prev = new_node
             self.head = new_node
             return
 
@@ -39,43 +44,61 @@ class SinglyLL:
             print(f"{index}th exist nahi karti")
 
         new_node.next = curr.next
+        if curr.next:
+            curr.next.prev = new_node
+        new_node.prev = curr
         curr.next = new_node
         return
 
-
     def remove(self, index: int) -> None:
+        if self.head is None:
+            print("List empty hai")
+            return
+
+        if index == 0:
+            self.head = self.head.next
+            if self.head:
+                self.head.prev = None
+            return
+
         curr_idx = 0
         curr = self.head
-        while curr_idx != (index-1):
+        while curr_idx != index - 1:
             if curr.next:
                 curr = curr.next
                 curr_idx += 1
             else:
-                print(f"LinkedList has size of {curr_idx+1} can't remove at {index} index")
+                print(
+                    f"LinkedList has size of {curr_idx + 1} can't remove at {
+                        index
+                    } index"
+                )
                 return
 
         curr.next = curr.next.next
+        if curr.next:
+            curr.next.prev = curr
         return
 
-    def pop(self) -> int|None:
+    def pop(self) -> int | None:
         if self.head is None:
-            print("list empty hai")
+            print("lisy empty hai")
             return None
 
         if self.head.next is None:
-            value = self.head.value
+            data = self.head.data
             self.head = None
-            return value
+            return data
 
         curr = self.head
         while curr.next.next:
             curr = curr.next
 
-        value = curr.next.value
+        data = curr.next.data
         curr.next = None
-        return value
+        return data
 
-    def get(self, index: int) -> int|str|None:
+    def get(self, index: int) -> int | str:
         if self.head is None:
             return "list empty hai"
         curr_idx = 0
@@ -87,42 +110,44 @@ class SinglyLL:
         if curr is None:
             return f"{index}th exist nahi karti"
 
-        return curr.value 
+        return curr.data
 
     def search(self, value: int) -> str:
         curr_idx = 0
         curr = self.head
-        while curr is not None and curr.value != value:
+        while curr is not None and curr.data != value:
             curr_idx += 1
             curr = curr.next
 
         if curr is None:
             return f"koi node nahi hai with value: {value}"
 
-        return f"yeh lo index: {curr_idx} aur uski value:{curr.value}"
+        return f"yeh lo index: {curr_idx} aur uski value:{curr.data}"
 
     def __str__(self):
         values = []
         curr = self.head
         while curr:
-            values.append(str(curr.value))
+            values.append(str(curr.data))
             curr = curr.next
-        return "->".join(values) + "->end"
 
-sll = SinglyLL()
-sll.add(5)
-sll.add(5)
-sll.add(3)
-sll.add(1)
-sll.add(2)
-sll.add(3)
-sll.add(1)
-sll.add(2)
-sll.insert(index=1, value=4)
-sll.remove(1)
-sll.pop()
-a = sll.get(0)
+        return " <-> ".join(values) + " <-> end"
+
+
+dll = DoublyLL()
+dll.add(5)
+dll.add(5)
+dll.add(3)
+dll.add(1)
+dll.add(2)
+dll.add(3)
+dll.add(1)
+dll.add(2)
+dll.insert(index=1, data=4)
+dll.remove(1)
+dll.pop()
+a = dll.get(0)
 print(a)
-b = sll.search(6)
+b = dll.search(6)
 print(b)
-print(sll)
+print(dll)
