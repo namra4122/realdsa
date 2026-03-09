be1502e4717663cddd8002f7d0af5ef649bc55b3
Namra Maniar <namra4122@gmail.com>
2026-03-09 18:06:13 +0530

    sorting: completed the quick sort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class QuickSort:
+    def __init__(self, data: List) -> None:
+        self.data = data
+
+    def _partition(self, l: int, h: int) -> int:
+        pivot = self.data[l]
+        b = l
+
+        for i in range(l + 1, h + 1):
+            if self.data[i] < pivot:
+                b += 1
+                self.data[i], self.data[b] = self.data[b], self.data[i]
+
+        self.data[l], self.data[b] = self.data[b], self.data[l]
+        return b
+
+    def _recur_sort(self, l: int, h: int):
+        if l < h:
+            pivot_iloc = self._partition(l, h)
+            self._recur_sort(l, pivot_iloc - 1)
+            self._recur_sort(pivot_iloc + 1, h)
+
+    def sort(self) -> List[int]:
+        self._recur_sort(0, len(self.data) - 1)
+        return self.data
+
+    def __str__(self) -> str:
+        return ",".join(map(str, self.data))
+
+
+data = QuickSort([22, 3, 4, 5, 1, 2, 388, 6, 9, 8, 0])
+data.sort()
+print(data)
